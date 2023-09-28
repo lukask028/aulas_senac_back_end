e02323096560d5fdf301c1523408cd439c4789d2
lukask028 <54475600+lukask028@users.noreply.github.com>
2023-09-28 09:26:46 -0300

    Add files via upload

---
+++ curso-back-end-senac/Arrayexibecalculonome/Arrayexibecalculonome/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ArrayNomeCalculo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+
+        // Digite 10 numeros
+        // Digite 10 nomes 
+        // Insira nos arrays , e exiba os 10 calculos e nomes 
+
+
+        // valores iniciais para os calculos:
+        int a, b, resultado, i;
+        string nome;
+        char operacao;
+
+        // declaracao do array
+        string[] array_nome_calculo = { };
+
+        /*Console.WriteLine("Insira o valor de a:");
+        a = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("Insira o valor de b:");
+        b = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine("a é : "+ a);
+        Console.WriteLine("b é : "+ b);*/
+
+
+        
+        for (i = 0; i < 10; i++)
+        {   
+            Console.WriteLine("Digite o nome:");
+            nome= Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor para a:");
+            a = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Digite uma operação: ");
+            operacao = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor para b:");
+            b = Convert.ToInt16(Console.ReadLine());
+
+            switch (operacao)
+            {
+                case (char)+ "+":
+                    resultado = (a + b);
+                    Console.WriteLine("Resultado:", resultado);
+                    break;
+
+            default:
+                Console.WriteLine("Saiu do teste");
+                
+                  break;
+
+            }
+        }
+
+        
+        
+
+    }
+}
---
+++ curso-back-end-senac/media3numeros/media3numeros/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace media3numeros;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        //Ex : media de 3 numeros
+        // entrada de 3 numeros,e o nome do usuario
+
+        // criacao do array com notas 
+        double[] notas = new double[5];
+
+        // var para soma dos elementos
+        double soma = 0;
+
+        // declaracao da varicao nome 
+        string nome;
+
+
+        
+        // adicionando notas no array 
+        for (int i = 0; i < notas.Length; i++)
+        {
+            // interporlação de strings "$" permite a construção de textos de maneira
+            // dinâmica, inserindo a variavel com valor, dentro de um texto.
+            Console.WriteLine($"Digite a nota{i} :");
+            notas[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+        // percorrendo o array 
+        for (int i = 0; i < notas.Length; i++)
+        {   
+            soma += notas[i];
+        }
+
+        Console.WriteLine("Algoritmo nome e medias:");
+
+        // entrada do nome
+        Console.WriteLine("digite seu nome:");
+        nome = Console.ReadLine();
+
+        // media dos arrays
+        double media = soma / notas.Length;
+
+        // formata a media 
+        string m = media.ToString("0.00");
+
+        // exibe o nome
+        Console.WriteLine("\nSeu nome: " + nome);
+
+        // Exibe a media do array
+        Console.WriteLine("media das notas:  " + m);
+    }
+}
